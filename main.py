9e731162860b6fa8a7ae324befb67835ef577536
silvadamaa <56432901+silvadamaa@users.noreply.github.com>
2019-10-11 16:38:17 +0700

    kecepatan tanpa fungsi

---
+++ main.py
@@ -0,0 +1,11 @@
+nama = 'Silva Damayanti'
+program = 'gerak lurus'
+
+print(f'Program {program} oleh {nama}')
+
+jarak = 1000
+waktu = 5 * 60
+kecepatan = jarak / waktu
+
+print (f'jarak = {jarak/1000}km ditempuh dalam waktu = {waktu/60}menit')
+print (f'sehingga kecepatan = {kecepatan} m/s')
